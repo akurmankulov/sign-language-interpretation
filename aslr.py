2d2f5a836626b47b882338f75d24cda8ce193854
Jianfei Zhang <ohsilence@hotmail.com>
2023-05-30 09:13:23 +0400

    Class for ASLRecognition is done

--- aslr.py
+++ aslr.py
@@ -16,8 +16,10 @@ from tensorflow.keras.backend import expand_dims
 # Usage:
 # from aslr import ASLRecognition
 # model = ASLRecognition()
-# model.loadmodel('model_path') where model_path is the location of model
-# model.predict(image) the image should be an numpy array with np.uint8 as dtype
+# model.loadmodel('model_path') where model_path is the location of model, and threshold for the probability can be specified. Default is 0.5
+# model.read_image() or model.read_array() to read the input as an jpg file or an array
+# model.preprocessing().predict(image) to do proprocessing and then predict. The input image size should
+# be specified in the preprocessing if not (300,300).
 #################################################
 
 
@@ -28,7 +30,9 @@ class  ASLRecognition:
     """
 
     def __init__(self):
-        self.letters_ = string.ascii_uppercase.replace('J','').replace('Z','')
+        # self.__dict__ = {'letters':string.ascii_uppercase.replace('J','').replace('Z','')}
+        self.__dict__ = {}
+        self.labels_ = list(string.ascii_uppercase.replace('J','').replace('Z',''))
 
     def loadmodel(self,model,threshold = 0.5):
         """
@@ -36,35 +40,49 @@ class  ASLRecognition:
         """
         self.model_ = load_model(model)
         self.threshold_ = threshold
+        self.__dict__['model'] = self.model_
         return self
 
-    def input_preprocessing(self, image, color_mode = 'rgb', img_size = (300,300), rescale = None):
+    def read_image(self,image):
+        """ If input is an image, then read the image to convert it to numpy array"""
+        self.__dict__['original_image'] = Image.open(image)
+        self.__dict__['RGB_image'] = self.__dict__['original_image'].convert('RGB')
+        self.image_ = np.array(self.__dict__['RGB_image'])
+        self.__dict__['array'] = self.image_
+        self.__dict__['resize'] = None
+        return self
+
+    def read_array(self,image_array):
+        self.__dict__['original_image'] = None
+        self.__dict__['RGB_image'] = None
+        self.__dict__['resize'] = None
+        self.image_ = image_array
+        self.__dict__['array'] = self.image_
+        return self
+
+    def preprocessing(self, img_size = (300,300), rescale = None):
         """
         The input image format should be a numpy array, and the rescale is the scaler used to divide the image for normalization
         """
-        # if color_mode != 'rgb':
-        #     img = np.array(Image.fromarray(image).convert('RGB'))
-        # else:
-        #     img = np.array(Image.fromarray(image))
 
-        img = np.array(image)
-
-        if img_size != img.shape[:2]:
-            img = cv2.resize(img,img_size)
+        if img_size != self.image_.shape[:2]:
+            self.image_ = self.image_.resize(img_size)
+            self.__dict__['resize'] = self.image_
 
         if rescale:
-            img  = img*1.0/rescale
+            self.image_ = self.image_*1.0/255
 
-        return expand_dims(img,axis = 0)
+        self.image_  = expand_dims(self.image_,axis = 0)
 
-    def predict(self, image, color_mode = 'rgb', img_size = (300,300), rescale = None):
-        X = self.input_preprocessing(image,color_mode=color_mode,img_size=img_size,rescale=rescale)
-        pred = self.model_.predict(X)
+        return self
+
+    def predict(self):
+        pred = self.model_.predict(self.image_)
         prob = np.max(pred)
         y = np.argmax(pred)
 
         if prob > self.threshold_:
-            return self.letters_[y],prob
+            return self.labels_[y],prob
         return  None
 
     def build_model(self):
