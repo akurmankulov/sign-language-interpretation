6a34654d0c63863ce9c70a29b2a365fc89dbca30
yakov kozlov <ykozlov@slb.com>
2023-05-30 20:51:51 +0800

    more tests added with 2 files

--- tests/test_script.py
+++ tests/test_script.py
@@ -9,6 +9,7 @@ sys.path.append('../')
 
 from aslr import ASLRecognition
 
+#you can specify a test variable aka fixture
 @pytest.fixture
 def path1():
     return "tests/test_image_A.jpg"
@@ -17,7 +18,13 @@ def path1():
 def path2():
     return "tests/test_image_U.jpg"
 
-path_cedric="tests/test_image_U.jpg"
+# it is possible to have a list of values and fixture will automatically run for each
+@pytest.fixture(params=["tests/test_image_A.jpg", "tests/test_image_F.jpg", "tests/test_image_U.jpg"])
+def path(request):
+    return request.param
+
+# a direct variable creation also works of course
+path_var="tests/test_image_F.jpg"
 
 @pytest.mark.parametrize('input_path , expected_type', [ ("tests/test_image_A.jpg", str), ("tests/test_image_U.jpg", str)] )
 def test_image_path(input_path, expected_type):
@@ -35,7 +42,7 @@ def test_image_path(input_path, expected_type):
     _a.read_image(input_path)
     assert type(input_path) == expected_type
 
-@pytest.mark.parametrize('input_path , shape', [ (path_cedric, (300, 300, 3)), ("tests/test_image_U.jpg", (300, 300, 3))] )
+@pytest.mark.parametrize('input_path , shape', [ (path_var, (200, 200, 3)), ("tests/test_image_U.jpg", (300, 300, 3))] )
 def test_image_orig_shape(input_path, shape):
     """
     Test the shape of the original image loaded by ASLRecognition.
@@ -52,7 +59,6 @@ def test_image_orig_shape(input_path, shape):
     _a.read_image(input_path)
     assert np.array(_a.__dict__['original_image']).shape == shape
 
-# @pytest.mark.parametrize('input_path , shape', [ ('path1', (300, 300, 3)), ('path2', (300, 300, 3))] )
 def test_image_np_shape(path1):
     """
     Test the shape of the image array loaded by ASLRecognition.
@@ -69,7 +75,24 @@ def test_image_np_shape(path1):
     _a.read_image(path1)
     assert _a.__dict__['array'].shape[0] == 300
 
+def test_many_images_np_shape(path):
+    """
+    Test the shape of the image array loaded by ASLRecognition.
 
+    Parameters:
+    path (str): The path to the image.
+
+    Raises:
+    AssertionError: If the shape of the image array loaded by ASLRecognition
+                    does not match the shape of the image loaded using PIL.
+
+    """
+    _a=ASLRecognition()
+    _a.read_image(path)
+    assert _a.__dict__['array'].shape[0] == np.array(Image.open(path)).shape[0]
+
+
+# this is no longer needed with latest pytest versions
 # if __name__ == "__main__":
     # print("Let the testing commence")
     # test_image_path(path)
