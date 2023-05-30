ad27edb0c4b0dfaf4242948e780afe3be8b91f1e
Alikhan Kurmankulov <akurmankulov@slb.com>
2023-05-30 11:17:39 +0600

    updated webcam 24

--- webcamera_app.py
+++ webcamera_app.py
@@ -1,33 +1,35 @@
 import streamlit as st
-import cv2
-from cvzone.HandTrackingModule import HandDetector
-from tensorflow.keras.models import load_model
+from streamlit_webrtc import webrtc_streamer
 import av
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration, VideoProcessorBase
-import numpy as np
-import math
+import cv2
 
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+
+class VideoProcessor:
+    def __init__(self) -> None:
+        self.threshold1 = 100
+        self.threshold2 = 200
+
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
-def callback(frame):
-    img = frame.to_ndarray(format="bgr24")
-
-    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
-
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
-
-webrtc_streamer(
+ctx = webrtc_streamer(
     key="example",
-    video_processor_factory=callback,
-    rtc_configuration={  # Add this line
+    video_processor_factory=VideoProcessor,
+    rtc_configuration={
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
     }
 )
+if ctx.video_processor:
+    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
+    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
 
 # detector = HandDetector(maxHands=1)
 # model = load_model("./base_line_model/base_cnn_model")
