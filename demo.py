47ec1516bd426922de6badf516769a2c177f92b2
Alikhan Kurmankulov <akurmankulov@slb.com>
2023-05-31 21:20:51 +0600

    test 2

--- demo.py
+++ demo.py
@@ -32,72 +32,71 @@ letters = {'0':'A', '1':'B', '2':'C', '3':'D', '4':'E', '5':'F', '6':'G', '7':'H
 
 ### App part
 st.title("Sign language interpreter")
-frame_placeholder = st.empty()
+#frame_placeholder = st.empty()
 
-class VideoProcessor:
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        imgOutput = img.copy()
-        hands, img = detector.findHands(img)
-        if hands:
-            hand = hands[0]
-            x, y, w, h = hand['bbox']
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+    imgOutput = img.copy()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
 
-            imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
-            imgCrop = img[y-offset : y+h+offset, x-offset : x+w+offset]
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
+        imgCrop = img[y-offset : y+h+offset, x-offset : x+w+offset]
 
-            aspectRatio = h/w
-            # try:
-            if aspectRatio > 1:
-                k = imgSize/h
-                wCal = math.ceil(k*w)
-                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
-                imgResizeShape = imgResize.shape
-                wGap = math.ceil((imgSize-wCal)/2)
-                imgWhite[:, wGap:wCal+wGap] = imgResize
-                # Preprocess the image
-                image_resized = cv2.resize(imgWhite, (imgSize_to_model, imgSize_to_model))
-                image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-                image_pre = preprocess_input(np.expand_dims(image_rgb, axis=0))  # Add batch dimension & Preprocess the input according to ResNet50 requirements
-                result = letters[str(int(np.argmax(model.predict(image_pre), axis=1)))]
-                # Add frame to the buffer
-                frame_buffer.append(result)
-                # Keep only the last `num_frames` frames in the buffer
-                if len(frame_buffer) > num_frames:
-                    frame_buffer = frame_buffer[-num_frames:]
-                # Once we have enough frames, make predictions
-                if len(frame_buffer) == num_frames:
-                    # Create a batch of frames
-                    final_predict = pd.DataFrame(frame_buffer).reset_index(drop=True).value_counts().keys()[0][0]
-            else:
-                k = imgSize/w
-                hCal = math.ceil(k*h)
-                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
-                imgResizeShape = imgResize.shape
-                hGap = math.ceil((imgSize-hCal)/2)
-                imgWhite[hGap:hCal+hGap, :] = imgResize
-                #prediction, index = classifier.getPrediction(imgWhite, draw=False)
-                image_resized = cv2.resize(imgWhite, (imgSize_to_model, imgSize_to_model))
-                image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
-                image_pre = preprocess_input(np.expand_dims(image_rgb, axis=0))  # Add batch dimension & Preprocess the input according to ResNet50 requirements
-                result = letters[str(int(np.argmax(model.predict(image_pre), axis=1)))]
-                # Add frame to the buffer
-                frame_buffer.append(result)
-                # Keep only the last `num_frames` frames in the buffer
-                if len(frame_buffer) > num_frames:
-                    frame_buffer = frame_buffer[-num_frames:]
-                # Once we have enough frames, make predictions
-                if len(frame_buffer) == num_frames:
-                    final_predict = pd.DataFrame(frame_buffer).reset_index(drop=True).value_counts().keys()[0][0]
-            # except:
-            #     continue
+        aspectRatio = h/w
+        # try:
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize-wCal)/2)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+            # Preprocess the image
+            image_resized = cv2.resize(imgWhite, (imgSize_to_model, imgSize_to_model))
+            image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+            image_pre = preprocess_input(np.expand_dims(image_rgb, axis=0))  # Add batch dimension & Preprocess the input according to ResNet50 requirements
+            result = letters[str(int(np.argmax(model.predict(image_pre), axis=1)))]
+            # Add frame to the buffer
+            frame_buffer.append(result)
+            # Keep only the last `num_frames` frames in the buffer
+            if len(frame_buffer) > num_frames:
+                frame_buffer = frame_buffer[-num_frames:]
+            # Once we have enough frames, make predictions
+            if len(frame_buffer) == num_frames:
+                # Create a batch of frames
+                final_predict = pd.DataFrame(frame_buffer).reset_index(drop=True).value_counts().keys()[0][0]
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k*h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize-hCal)/2)
+            imgWhite[hGap:hCal+hGap, :] = imgResize
+            #prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            image_resized = cv2.resize(imgWhite, (imgSize_to_model, imgSize_to_model))
+            image_rgb = cv2.cvtColor(image_resized, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+            image_pre = preprocess_input(np.expand_dims(image_rgb, axis=0))  # Add batch dimension & Preprocess the input according to ResNet50 requirements
+            result = letters[str(int(np.argmax(model.predict(image_pre), axis=1)))]
+            # Add frame to the buffer
+            frame_buffer.append(result)
+            # Keep only the last `num_frames` frames in the buffer
+            if len(frame_buffer) > num_frames:
+                frame_buffer = frame_buffer[-num_frames:]
+            # Once we have enough frames, make predictions
+            if len(frame_buffer) == num_frames:
+                final_predict = pd.DataFrame(frame_buffer).reset_index(drop=True).value_counts().keys()[0][0]
+        # except:
+        #     continue
 
-            cv2.rectangle(imgOutput, (x-offset, y-offset-50), (x-offset+90, y-offset), (255, 0, 255), cv2.FILLED)
-            cv2.putText(imgOutput, final_predict, (x, y-27), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255,255,255), 2)
-            cv2.rectangle(imgOutput, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 255), 4)
+        cv2.rectangle(imgOutput, (x-offset, y-offset-50), (x-offset+90, y-offset), (255, 0, 255), cv2.FILLED)
+        cv2.putText(imgOutput, final_predict, (x, y-27), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255,255,255), 2)
+        cv2.rectangle(imgOutput, (x-offset, y-offset), (x+w+offset, y+h+offset), (255, 0, 255), 4)
 
-        frame = cv2.cvtColor(imgOutput, cv2.COLOR_BGR2RGB)
-        return av.VideoFrame.from_ndarray(frame) #, format="bgr24"
+    frame_pr = cv2.cvtColor(imgOutput, cv2.COLOR_BGR2RGB)
+    return av.VideoFrame.from_ndarray(frame_pr) #, format="bgr24"
 
 webrtc_ctx = webrtc_streamer(
     key="object-detection",
@@ -106,7 +105,7 @@ webrtc_ctx = webrtc_streamer(
         "iceServers": get_ice_servers(),
         "iceTransportPolicy": "relay",
     },
-    video_processor_factory=VideoProcessor,
+    video_frame_callback=callback,
     media_stream_constraints={"video": True, "audio": False},
     async_processing=True,
 )
